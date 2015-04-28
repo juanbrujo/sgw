ee1c3d58b8ea6d6710a144727bd4556143c09877
Jorge Epuñan <juanbrujo@gmail.com>
2015-04-28 18:17:36 -0400

    new 0.1.0 version
    - added prompt questions
    - added progress bar

--- bin/index.js
+++ bin/index.js
@@ -1,28 +1,45 @@
 #!/usr/bin/env node
 
-var path 			= require('path'),
-		git 			= require("nodegit"),
-		fs  			= require('fs'),
-		program  	= require('commander'),
-		pkg 			= require(path.join(__dirname, '../package.json'));
+var path        = require('path'),
+		git 		    = require("nodegit"),
+		fs  		    = require('fs'),
+		program     = require('commander'),
+    inquirer    = require('inquirer'),
+    pkg 		    = require(path.join(__dirname, '../package.json')),
+    progress    = require('progress');
 
 program
 	.version(pkg.version)
 	.option('-s, --start [projectname]', 'Project name (required)')
-	.option('-v, --verbose', '')
 	.parse(process.argv);
 
-if (!program.start) {
-	console.log('You need a project name');
-	return false;
-}
+var questions = [
+  {
+    type: 'input',
+    name: 'projectName',
+    message: 'Project name:',
+    validate: function( value ) {
+      if (value) {
+        return true;
+      } else {
+        return "Please enter a name for your project";
+      }
+    }
+  }
+]
 
-if (fs.existsSync(program.start)) {
-	console.log('Project name already exists; choose a new one');
-	return false;
+function ask() {
+  inquirer.prompt( questions, function( answers ) {
+    var projectName = answers.projectName.toLowerCase().replace(/[^a-zA-Z0-9]/g,'_');
+    if (fs.existsSync(projectName)) {
+      console.log('Project name already exists; choose a new one');
+      ask();
+    } else {
+      getRepo(projectName);
+    }
+  });
 }
-
-var projectName = program.start.toLowerCase().replace(/[^a-zA-Z0-9]/g,'_');
+ask();
 
 var cloneOptions = {};
 cloneOptions.remoteCallbacks = {
@@ -33,10 +50,16 @@ cloneOptions.remoteCallbacks = {
   credentials: function(url, userName) {
     return git.Cred.sshKeyFromAgent(userName);
   }
-  // ,
-  // transferProgress: function(info) {
-  //   return console.log(info);
-  // }
+  ,
+  transferProgress: function(info) {
+    var bar = new progress(':bar', { total: 10 });
+    var timer = setInterval(function () {
+      bar.tick();
+      if (bar.complete) {
+        clearInterval(timer);
+      }
+    }, 100);
+  }
 };
 
 var errorAndAttemptOpen = function() {
@@ -44,12 +67,11 @@ var errorAndAttemptOpen = function() {
 };
 
 function getRepo(projectName){
+  console.log('╔═╗╔═╗╦ ╦\r\n╚═╗║ ╦║║║  ~~~ Simple Grunt Workflow\r\n╚═╝╚═╝╚╩╝');
 	git.Clone("https://github.com/juanbrujo/simple-grunt-workflow", projectName, cloneOptions)
 	.catch(errorAndAttemptOpen)
   .done(function() {
-      console.log('Ready to go! now `$ cd ' + projectName + '` and start installing your npm and bower libs');
+      console.log('\nReady to go! Now `$ cd ' + projectName + '` and start installing your npm and bower libs');
       process.exit();
   })
 }
-
-getRepo(projectName);
